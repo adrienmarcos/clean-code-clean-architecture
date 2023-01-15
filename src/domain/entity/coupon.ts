e824cbba72735a0dd73423144d16cb5bc538bc97
Adrien Rossato <adrien.marcos@gmail.com>
2023-01-15 20:32:03 -0300

    feat: ensure Coupon create a valid coupon if correct data is provided

--- src/domain/entity/coupon.ts
+++ src/domain/entity/coupon.ts
@@ -2,8 +2,13 @@ export default class Coupon {
   readonly code: string
   readonly percentage: number
 
-  constructor (code: string, percentage: number) {
+  constructor (code: string, percentage: number, readonly expireDate?: Date) {
     this.code = code
     this.percentage = percentage
+  }
+
+  isValid (curretDate: Date): boolean {
+    if (!this.expireDate) return true
+    return curretDate.getTime() <= this.expireDate.getTime()
   }
 }
