023ee50dd3811ca3266545edd640e86c3532d232
Adrien Rossato <adrien.marcos@gmail.com>
2023-01-23 16:50:54 -0300

    test: ensure PlaceOrder places an order with code if valid data is provided

--- test/integration/place-order.test.ts
+++ test/integration/place-order.test.ts
@@ -22,4 +22,24 @@ describe('Place Order', () => {
     const output = await placeOrder.execute(input)
     expect(output.total).toBe(72)
   })
+
+  test('Should place an Order with a OrderCode if valid data is provided', async () => {
+    const itemRepository = new ItemRepositoryMemory()
+    const orderRepository = new OrderRepositoryMemory()
+    const couponRepository = new CouponRepositoryMemory()
+    const placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository)
+    const input = {
+      cpf: '839.435.452-10',
+      orderItems: [
+        { idItem: 1, quantity: 1 },
+        { idItem: 2, quantity: 1 },
+        { idItem: 3, quantity: 1 }
+      ],
+      date: new Date('2023-1-16'),
+      coupon: 'Vale20'
+    }
+    const output = await placeOrder.execute(input)
+    expect(output.total).toBe(72)
+    expect(output.code).toBe('202300000001')
+  })
 })
