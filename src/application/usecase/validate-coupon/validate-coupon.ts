9c66650243443744c24aa8df24c7cd3ed03740ff
Adrien Rossato <adrien.marcos@gmail.com>
2023-01-24 13:19:21 -0300

    feat: ensure ValidateCoupon validates a valid coupon

---
+++ src/application/usecase/validate-coupon/validate-coupon.ts
@@ -0,0 +1,15 @@
+import { CouponRepository } from '../../../domain/repository/coupon-repository'
+
+export class ValidateCoupon {
+  readonly couponRepository: CouponRepository
+
+  constructor (couponRepository: CouponRepository) {
+    this.couponRepository = couponRepository
+  }
+
+  async execute (code: string): Promise<boolean> {
+    const coupon = await this.couponRepository.findByCode(code)
+    if (!coupon) throw new Error('Coupon not found')
+    return coupon.isValid(new Date())
+  }
+}
