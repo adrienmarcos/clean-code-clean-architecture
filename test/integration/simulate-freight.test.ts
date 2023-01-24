231f12bc68aa0230686d30da19365a5a2c23f900
Adrien Rossato <adrien.marcos@gmail.com>
2023-01-24 13:00:58 -0300

    test: ensure SimulateFreight simulates the items's freight

---
+++ test/integration/simulate-freight.test.ts
@@ -0,0 +1,21 @@
+import { SimulateFreightInput } from '../../src/application/usecase/simulate-freight/simulate-freight-input'
+import { SimulateFreight } from '../../src/application/usecase/simulate-freight/simulate-freigth'
+import { DefaultFreightCalculator } from '../../src/domain/entity/default-freight-calculator'
+import { PgPromiseConnectionAdapter } from '../../src/infrastructure/database/pg-promise-connections-adapter'
+import { ItemRepositoryDatabase } from '../../src/infrastructure/repository/database/item-repository-database'
+
+describe('Simulate Freight', () => {
+  test('Should simulate the freight of items', async () => {
+    const connection = new PgPromiseConnectionAdapter()
+    const itemRepository = new ItemRepositoryDatabase(connection)
+    const freightCalculator = new DefaultFreightCalculator()
+    const simulateFreight = new SimulateFreight(itemRepository, freightCalculator)
+    const input = new SimulateFreightInput([
+      { idItem: 4, quantity: 1 },
+      { idItem: 5, quantity: 1 },
+      { idItem: 6, quantity: 3 }
+    ])
+    const output = await simulateFreight.execute(input)
+    expect(output.amount).toBe(260)
+  })
+})
