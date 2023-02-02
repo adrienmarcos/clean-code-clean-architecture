1df57100df71292b045145d026efe350af8f8c91
Adrien Rossato <adrien.marcos@gmail.com>
2023-02-02 20:14:03 -0300

    feat: remove express dependecy from the api

---
+++ src/infrastructure/http/express-adapter.ts
@@ -0,0 +1,22 @@
+import Express from 'express'
+import Http from './http'
+
+export default class ExpressAdapter implements Http {
+  private readonly app: any
+
+  constructor () {
+    this.app = Express()
+    this.app.use(Express.json())
+  }
+
+  on (url: string, method: string, fn: any): void {
+    this.app[method](url, async (req: any, res: any) => {
+      const output = await fn(req.params, req.body)
+      res.json(output)
+    })
+  }
+
+  listen (port: number): void {
+    this.app.listen(port)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,24 @@
-import Express from 'express'
 import { PlaceOrder } from './application/usecase/place-order/place-order'
 import { SimulateFreight } from './application/usecase/simulate-freight/simulate-freigth'
 import { DefaultFreightCalculator } from './domain/entity/default-freight-calculator'
 import { PgPromiseConnectionAdapter } from './infrastructure/database/pg-promise-connections-adapter'
 import { DatabaseRepositoryFactory } from './infrastructure/factory/database-repository-factory'
 import { ItemRepositoryDatabase } from './infrastructure/repository/database/item-repository-database'
+import ExpressAdapter from './infrastructure/http/express-adapter'
 
-const app = Express()
-app.use(Express.json())
-
-app.post('/orders', async (req, res) => {
+const expressAdapter = new ExpressAdapter()
+expressAdapter.on('/orders', 'post', async (params: any, body: any) => {
   const placeOrder = new PlaceOrder(new DatabaseRepositoryFactory())
-  const input = req.body
+  const input = body
   input.date = new Date(input.date)
-  const output = await placeOrder.execute(input)
-  res.json(output)
+  return await placeOrder.execute(input)
 })
 
-app.post('/simulateFreight', async (req, res) => {
+expressAdapter.on('/simulateFreight', 'post', async (params: any, body: any) => {
   const itemRepository = new ItemRepositoryDatabase(PgPromiseConnectionAdapter.getInstance())
   const simulateFreight = new SimulateFreight(itemRepository, new DefaultFreightCalculator())
-  const input = req.body
-  const output = await simulateFreight.execute(input)
-  res.json(output)
+  const input = body
+  return await simulateFreight.execute(input)
 })
 
-app.listen(3000)
+expressAdapter.listen(3000)
