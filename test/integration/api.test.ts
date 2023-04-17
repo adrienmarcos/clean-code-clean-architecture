dee364f7859673ad96b9665530e3a91149378076
Adrien Rossato <adrien.marcos@gmail.com>
2023-04-17 15:05:17 -0300

    test: ensure GetOrder and GetOrders get Orders if valid data is provided

--- test/integration/api.test.ts
+++ test/integration/api.test.ts
@@ -1,4 +1,18 @@
 import axios from 'axios'
+import { PlaceOrder } from '../../src/application/usecase/place-order/place-order'
+import { PgPromiseConnectionAdapter } from '../../src/infrastructure/database/pg-promise-connections-adapter'
+import { DatabaseRepositoryFactory } from '../../src/infrastructure/factory/database-repository-factory'
+import { OrderRepositoryDatabase } from '../../src/infrastructure/repository/database/order-repository-database'
+
+let placeOrder: PlaceOrder
+let orderRepository: OrderRepositoryDatabase
+
+beforeEach(() => {
+  const connection = PgPromiseConnectionAdapter.getInstance()
+  orderRepository = new OrderRepositoryDatabase(connection)
+  const repositoryFactory = new DatabaseRepositoryFactory()
+  placeOrder = new PlaceOrder(repositoryFactory)
+})
 
 describe('Api', () => {
   test('Should test the Api at url /orders using POST method', async () => {
@@ -35,4 +49,28 @@ describe('Api', () => {
     const output = response.data
     expect(output.amount).toBe(260)
   })
+
+  test('Should test the Api at url /orders using GET method', async () => {
+    const input = {
+      cpf: '839.435.452-10',
+      orderItems: [
+        { idItem: 1, quantity: 1 },
+        { idItem: 2, quantity: 1 },
+        { idItem: 3, quantity: 3 }
+      ],
+      date: new Date('2023-1-16'),
+      coupon: 'VALE20'
+    }
+    await placeOrder.execute(input)
+    const response = await axios({
+      url: 'http://localhost:3000/orders',
+      method: 'get'
+    })
+    const output = response.data
+    console.log(output)
+  })
+})
+
+afterEach(async () => {
+  await orderRepository.clear()
 })
